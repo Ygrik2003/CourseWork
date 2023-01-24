25041f31eb04d1a326aaa1db17997032b4a64962
Ygrik2003 <ygrik2003@yandex.ru>
2023-01-24 15:36:34 +0300

    Add support the different versions and atempts for model

--- src/logistic_model/storage_model.py
+++ src/logistic_model/storage_model.py
@@ -4,10 +4,10 @@ from datetime import datetime
 import os.path
 from os import walk
 import pickle
-
+import re
 
 path = '..\\..\\Results\\'
-def get_format_name(attempt, version, file_format, date = datetime.now().date()): 
+def get_format_name(version, attempt, file_format, date = datetime.now().date()): 
     return f'result_{version}_{attempt}_{date}{file_format}'
 
 #file formats
@@ -16,25 +16,37 @@ info_file_format = '.info'
 jpg_file_format = '.jpg'
 
 
-def save_model(result : GridSearchCV):
-    attempt, version = 0, 0
-    with open(os.path.join(path, get_format_name(attempt, version, model_file_format)), 'xb') as model:
+def save_model(result : GridSearchCV, version=None):
+    last_version = -1
+    last_attempt = -1
+
+    filenames = next(walk(path))[2]
+    for filename in filenames:
+        match = re.search('result_(\d+)_(\d+)', filename)
+        if version == None:
+            if last_version < int(match[1]):
+                last_version = int(match[1])
+
+        elif version == int(match[1]):
+                last_attempt = max(last_attempt, int(match[2]))
+                
+    version = version if version != None else last_version + 1
+    attempt = last_attempt + 1
+    with open(os.path.join(path, get_format_name(version, attempt, model_file_format)), 'xb') as model:
         model.write(pickle.dumps(result))
 
 
-def load_model(attempt, version):
-    # if not os.path.isfile(os.path.join(path, file_name)):
-    #     raise Exception('This file doesn\'t exist')
+def load_model(version, attempt):
     filenames = next(walk(path))[2]
     for filename in filenames:
-        if get_format_name(attempt, version, '', '') in filename:
-            date = filename[len(get_format_name(attempt, version, '', '')):len(get_format_name(attempt, version, '', '')) + 10]
-            print(date)
+        match = re.search('result_(\d+)_(\d+)_(\d{4})-(\d{2})-(\d{2})', filename)
+        if int(match[1]) == version and int(match[2]) == attempt:
+            date = datetime(int(match[3]), int(match[4]), int(match[5]))
             break
     else:
         raise Exception('This file doesn\'t exist')
 
-    with open(os.path.join(path, get_format_name(attempt, version, model_file_format, date)), 'rb') as model:
+    with open(os.path.join(path, get_format_name(version, attempt, model_file_format, date.date())), 'rb') as model:
         result = pickle.loads(model.read())
     
     return result
